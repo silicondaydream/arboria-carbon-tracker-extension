2701c53cad2a3ab2407b4d85a5d7a525810bb391
Chris <accounts@chrisadams.io>
2024-02-06 14:21:12 -0500

    CI: visualization and data handling add

---
+++ functionality/background.js
@@ -0,0 +1,60 @@
+let storage = window.localStorage;
+let _dayPrefix = 'p';
+let _dailyCount = 0;
+let _keepDays = 45;
+
+function formatDate(prev, sep) {
+	sep = sep ? sep : "";
+	let now = new Date();
+	let td = new Date(now.getFullYear(), now.getMonth(), now.getDate() - prev);
+	let dateParts = [
+		('0000' + td.getFullYear()).slice(-4),		// year: YYYY
+		('00' + (td.getMonth() + 1)).slice(-2),		// month: MM
+		('00' + td.getDate()).slice(-2)				// day: DD
+	]
+	return dateParts.join(sep);
+}
+
+function getDayKey(d) {
+	return _dayPrefix + d;
+}
+
+function setDayCount(previous, obj) { // Put day's pageviews into storage
+	storage.setItem(getDayKey(formatDate(previous)), obj);
+}
+
+function getDayCount(previous) { // Pull day's pageviews from storage
+	let c = storage.getItem(getDayKey(formatDate(previous)));
+	return c == null ? 0 : parseInt(c);
+}
+
+
+// localStorage key lifecycle management
+function getAllKeys(prefix) { // Get all keys with `prefix`
+	return Object.keys(storage).filter(x => x.startsWith(prefix))
+}
+
+function getOldDayKeys(keep) {	// Get array of keys older than `keep`
+	let newKeys = [];
+	for (let i = keep - 1; i >= 0; i--) {
+		newKeys.push(getDayKey(formatDate(i)));
+	}
+	return getAllKeys(_dayPrefix).filter(x => !newKeys.includes(x));
+}
+
+function purgeOldKeys(keep) { // Delete all keys older than `keep`
+	getOldDayKeys(keep).forEach(function (item, index) { storage.removeItem(item) });
+}
+
+
+// Load listeners
+chrome.extension.onRequest.addListener(function (f, s, r) {
+	_dailyCount = getDayCount(0) + 1;
+	setDayCount(0, _dailyCount);
+});
+
+chrome.extension.onRequest.addListener(function (f, s, r) {
+	if (getAllKeys(_dayPrefix).length > _keepDays) {
+		purgeOldKeys(_keepDays);
+	}
+});
